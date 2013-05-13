b50df317d365f1841ae1f1ddc86bf4c6bd5a71a8
Joel Kåberg <joel.kaberg@gmail.com>
2013-05-13 18:48:16 +0200

    added GPL 3 license

--- uProcess.py
+++ uProcess.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-# Written by jkaberg, https://github.com/jkaberg
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    
+#    Author: jkaberg, https://github.com/jkaberg
+
 import os
 import sys
 import time
